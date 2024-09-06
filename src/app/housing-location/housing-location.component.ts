0757e729c7a1857ab3238a29a9a67717687eeb32
Sergio Terroso <sergio.terroso@gmail.com>
2024-09-06 13:32:35 -0600

    section 05: Added the Input decorator into HousingLocationComponent.

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housing-location';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   template: `
     <p>
       housing-location works!
@@ -12,5 +14,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./housing-location.component.css']
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: HousingLocation;
 }
