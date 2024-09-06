05d64bd17b3d224122a7f4352dd578efafc6904e
Sergio Terroso <sergio.terroso@gmail.com>
2024-09-06 14:40:27 -0600

    section 10: enabled routing and defined new routes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import {Component} from '@angular/core';
-
-import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent],
+  imports: [RouterModule],
   template: `
     <main>
       <header class="brand-name">
@@ -13,7 +12,7 @@ import { HomeComponent } from './home/home.component';
       </header>
 
       <section class="content">
-        <app-home></app-home>
+        <router-outlet></router-outlet>
       </section>
     </main>
   `,
