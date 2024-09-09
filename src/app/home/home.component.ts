7cbbd04fd6fb4f7766efd0193293020c200ffa68
Sergio Terroso <sergio.terroso@gmail.com>
2024-09-08 23:59:53 -0600

    section 13: used template variables to interact with template values, and add search functionality using event binding and array functions.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,14 +12,14 @@ import { HousingLocation } from '../housing-location';
   template: `
     <section>
       <form>
-        <input type="text" placeholder="Filter by city" />
-        <button class="primary" type="button">Search</button>
+        <input type="text" placeholder="Filter by city" #filter />
+        <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
       </form>
     </section>
 
     <section class="results">
       <app-housing-location
-        *ngFor="let housingLocation of housingLocationList"
+        *ngFor="let housingLocation of filteredLocationList"
           [housingLocation]="housingLocation"
       ></app-housing-location>
     </section>
@@ -29,8 +29,19 @@ import { HousingLocation } from '../housing-location';
 export class HomeComponent {
   private _housingService: HousingService = inject(HousingService);
   housingLocationList: HousingLocation[];
+  filteredLocationList: HousingLocation[] = [];
 
   constructor() {
     this.housingLocationList = this._housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+
+  filterResults(text: string): void {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter((location) => location.city.toLowerCase().includes(text.toLowerCase()));
   }
 }
